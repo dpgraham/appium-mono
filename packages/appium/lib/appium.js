bem/appium

Simplify getDeviceType() with tests rewritten

lib/appium.js
@@ -117,25 +117,16 @@ Appium.prototype.start = function (desiredCaps, cb) {
 };
 
 Appium.prototype.getDeviceType = function (args, caps) {
-  // TODO: 'deviceName' and 'platformName' caps are now required, so the majority of this code could be removed
-  var type =  this.getDeviceFromMJSONWP(args, caps) ||
-              this.getDeviceTypeFromArgsOrNamedApp(args, caps) ||
-              this.getDeviceTypeFromAppOrPackage(args, caps);
-  if (type) {
-    return type;
-  }
-  throw new Error("Could not determine your device from Appium arguments " +
-                "or desired capabilities. Please make sure to specify the " +
-                "'deviceName' and 'platformName' capabilities");
-};
-
-Appium.prototype.getDeviceFromMJSONWP = function (args, caps) {
   var platform = caps.platformName || args.platformName;
   platform = platform ? platform.toString().toLowerCase() : '';
   var browser = caps.browserName || args.browserName;
   browser = browser ? browser.toString().toLowerCase() : '';
   var automation = caps.automationName || args.automationName;
   automation = automation ? automation.toString().toLowerCase() : '';
+  var app = args.app || caps.app;
+  app = app ? app.toString().toLowerCase() : '';
+  var pkg = args.androidPackage || caps.appPackage;
+  pkg = pkg ? pkg.toString().toLowerCase() : '';
 
   var validPlatforms = ['ios', 'android', 'firefoxos'];
   if (platform && !_.contains(validPlatforms, platform)) {
@@ -144,43 +135,62 @@ Appium.prototype.getDeviceFromMJSONWP = function (args, caps) {
                     "is not a supported platform. Supported platforms are: " +
                     "iOS, Android and FirefoxOS");
   }
-  var type =  this.getDeviceTypeFromAutomationName(automation) ||
-              this.getDeviceTypeFromBrowserName(browser) ||
-              this.getDeviceTypeFromPlatformCap(platform);
-  if (type) return type;
-};
 
-Appium.prototype.getDeviceTypeFromArgsOrNamedApp = function (args, caps) {
-  var app = args.app || caps.app;
-  app = app ? app.toString().toLowerCase() : '';
-  var type = this.getDeviceTypeFromNamedApp(app) ||
-             this.getDeviceTypeFromArgs(args);
-  if (type) return type;
-};
-
-Appium.prototype.getDeviceTypeFromAppOrPackage = function (args, caps) {
-  var app = args.app || caps.app;
-  app = app ? app.toString().toLowerCase() : '';
-  var pkg = args.androidPackage || caps.appPackage;
-  pkg = pkg ? pkg.toString().toLowerCase() : '';
-  var type =  this.getDeviceTypeFromApp(app) ||
-              this.getDeviceTypeFromPackage(pkg);
-  if (type) return type;
-};
-
-Appium.prototype.getDeviceTypeFromAutomationName = function (automation) {
-  if (automation.indexOf('selendroid') !== -1) return DT_SELENDROID;
-};
-
-Appium.prototype.getDeviceTypeFromBrowserName = function (browser) {
-  if (browser === "safari") {
-    return DT_SAFARI;
-  } else if (_.contains(["chrome", "chromium", "chromebeta", "browser"], browser)) {
-    return DT_CHROME;
+  // TODO: Set DT_SELENDROID type based on platformVersion
+  var type = this.getDeviceTypeFromPlatform(platform);
+  if (type === DT_ANDROID) {
+    // Detect Chrome browser from browserName, app and pkg
+    if (
+        this.isChromeBrowser(browser) ||
+        this.isChromeBrowser(app) ||
+        this.isChromePackage(pkg)
+      ) {
+      type = DT_CHROME;
+    } else if (this.isSelendroidAutomation(automation)) {
+      type = DT_SELENDROID;
+    }
+  } else if (type === DT_IOS) {
+    // Detect Safari browser from browserName, app and args
+    if (
+        args.safari ||
+        this.isSafariBrowser(browser) ||
+        this.isSafariBrowser(app)
+      ) {
+      type = DT_SAFARI;
+    }
   }
+
+  if (!type) {
+    throw new Error("Could not determine your device from Appium arguments " +
+                  "or desired capabilities. Please make sure to specify the " +
+                  "'deviceName' and 'platformName' capabilities");
+  }
+  return type;
 };
 
-Appium.prototype.getDeviceTypeFromPlatformCap = function (caps) {
+Appium.prototype.isSelendroidAutomation = function (automation) {
+  return automation.indexOf('selendroid') !== -1;
+};
+
+Appium.prototype.isChromeBrowser = function (browser) {
+  return _.contains(["chrome", "chromium", "chromebeta", "browser"], browser);
+};
+
+Appium.prototype.isChromePackage = function (pkg) {
+  var chromePkgs = [
+    "com.android.chrome"
+  , "com.chrome.beta"
+  , "org.chromium.chrome.testshell"
+  , "com.android.browser"
+  ];
+  return _.contains(chromePkgs, pkg);
+};
+
+Appium.prototype.isSafariBrowser = function (browser) {
+  return browser === "safari";
+};
+
+Appium.prototype.getDeviceTypeFromPlatform = function (caps) {
   var device = null;
   switch (caps) {
   case 'ios':
@@ -194,63 +204,6 @@ Appium.prototype.getDeviceTypeFromPlatformCap = function (caps) {
     break;
   }
   return device;
-};
-
-Appium.prototype.getDeviceTypeFromDeviceCap = function (device) {
-
-  if (device.indexOf('iphone') !== -1) {
-    return DT_IOS;
-  } else if (device.indexOf('ipad') !== -1) {
-    return DT_IOS;
-  } else if (device.indexOf('selendroid') !== -1) {
-    return DT_SELENDROID;
-  } else if (device.indexOf('firefox') !== -1) {
-    return DT_FIREFOX_OS;
-  } else if (device.indexOf('android') !== -1) {
-    return DT_ANDROID;
-  }
-};
-
-Appium.prototype.getDeviceTypeFromNamedApp = function (app) {
-  if (app  === "safari") {
-    return DT_SAFARI;
-  } else if (app === "settings") {
-    return DT_IOS;
-  } else if (_.contains(["chrome", "chromium", "chromebeta", "browser"], app)) {
-    return DT_CHROME;
-  }
-};
-
-Appium.prototype.getDeviceTypeFromApp = function (app) {
-  if (/\.app$/.test(app) || /\.app\.zip$/.test(app)) {
-    return DT_IOS;
-  } else if (/\.apk$/.test(app) || /\.apk\.zip$/.test(app)) {
-    return DT_ANDROID;
-  }
-};
-
-Appium.prototype.getDeviceTypeFromPackage = function (pkg) {
-  var chromePkgs = [
-    "com.android.chrome"
-  , "com.chrome.beta"
-  , "org.chromium.chrome.shell"
-  , "com.android.browser"
-  ];
-  if (_.contains(chromePkgs, pkg)) {
-    return DT_CHROME;
-  } else if (pkg) {
-    return DT_ANDROID;
-  }
-};
-
-Appium.prototype.getDeviceTypeFromArgs = function (args) {
-  if (args.ipa && /\.ipa$/.test(args.ipa.toString().toLowerCase())) {
-    return DT_IOS;
-  } else if (args.safari) {
-    return DT_SAFARI;
-  } else if (args.forceIphone || args.forceIpad) {
-    return DT_IOS;
-  }
 };
 
 Appium.prototype.configure = function (args, desiredCaps, cb) {

dpgraham/appium-mono

packages/appium/lib/appium.js
@@ -130,25 +130,16 @@ Appium.prototype.start = function (desiredCaps, cb) {
 };
 
 Appium.prototype.getDeviceType = function (args, caps) {
-  // TODO: 'deviceName' and 'platformName' caps are now required, so the majority of this code could be removed
-  var type =  this.getDeviceFromMJSONWP(args, caps) ||
-              this.getDeviceTypeFromArgsOrNamedApp(args, caps) ||
-              this.getDeviceTypeFromAppOrPackage(args, caps);
-  if (type) {
-    return type;
-  }
-  throw new Error("Could not determine your device from Appium arguments " +
-                "or desired capabilities. Please make sure to specify the " +
-                "'deviceName' and 'platformName' capabilities");
-};
-
-Appium.prototype.getDeviceFromMJSONWP = function (args, caps) {
   var platform = caps.platformName || args.platformName;
   platform = platform ? platform.toString().toLowerCase() : '';
   var browser = caps.browserName || args.browserName;
   browser = browser ? browser.toString().toLowerCase() : '';
   var automation = caps.automationName || args.automationName;
   automation = automation ? automation.toString().toLowerCase() : '';
+  var app = args.app || caps.app;
+  app = app ? app.toString().toLowerCase() : '';
+  var pkg = args.androidPackage || caps.appPackage;
+  pkg = pkg ? pkg.toString().toLowerCase() : '';
 
   var validPlatforms = ['ios', 'android', 'firefoxos'];
   if (platform && !_.contains(validPlatforms, platform)) {
@@ -157,43 +148,62 @@ Appium.prototype.getDeviceFromMJSONWP = function (args, caps) {
                     "is not a supported platform. Supported platforms are: " +
                     "iOS, Android and FirefoxOS");
   }
-  var type =  this.getDeviceTypeFromAutomationName(automation) ||
-              this.getDeviceTypeFromBrowserName(browser) ||
-              this.getDeviceTypeFromPlatformCap(platform);
-  if (type) return type;
-};
 
-Appium.prototype.getDeviceTypeFromArgsOrNamedApp = function (args, caps) {
-  var app = args.app || caps.app;
-  app = app ? app.toString().toLowerCase() : '';
-  var type = this.getDeviceTypeFromNamedApp(app) ||
-             this.getDeviceTypeFromArgs(args);
-  if (type) return type;
-};
-
-Appium.prototype.getDeviceTypeFromAppOrPackage = function (args, caps) {
-  var app = args.app || caps.app;
-  app = app ? app.toString().toLowerCase() : '';
-  var pkg = args.androidPackage || caps.appPackage;
-  pkg = pkg ? pkg.toString().toLowerCase() : '';
-  var type =  this.getDeviceTypeFromApp(app) ||
-              this.getDeviceTypeFromPackage(pkg);
-  if (type) return type;
-};
-
-Appium.prototype.getDeviceTypeFromAutomationName = function (automation) {
-  if (automation.indexOf('selendroid') !== -1) return DT_SELENDROID;
-};
-
-Appium.prototype.getDeviceTypeFromBrowserName = function (browser) {
-  if (browser === "safari") {
-    return DT_SAFARI;
-  } else if (_.contains(["chrome", "chromium", "chromebeta", "browser"], browser)) {
-    return DT_CHROME;
+  // TODO: Set DT_SELENDROID type based on platformVersion
+  var type = this.getDeviceTypeFromPlatform(platform);
+  if (type === DT_ANDROID) {
+    // Detect Chrome browser from browserName, app and pkg
+    if (
+        this.isChromeBrowser(browser) ||
+        this.isChromeBrowser(app) ||
+        this.isChromePackage(pkg)
+      ) {
+      type = DT_CHROME;
+    } else if (this.isSelendroidAutomation(automation)) {
+      type = DT_SELENDROID;
+    }
+  } else if (type === DT_IOS) {
+    // Detect Safari browser from browserName, app and args
+    if (
+        args.safari ||
+        this.isSafariBrowser(browser) ||
+        this.isSafariBrowser(app)
+      ) {
+      type = DT_SAFARI;
+    }
   }
+
+  if (!type) {
+    throw new Error("Could not determine your device from Appium arguments " +
+                  "or desired capabilities. Please make sure to specify the " +
+                  "'deviceName' and 'platformName' capabilities");
+  }
+  return type;
 };
 
-Appium.prototype.getDeviceTypeFromPlatformCap = function (caps) {
+Appium.prototype.isSelendroidAutomation = function (automation) {
+  return automation.indexOf('selendroid') !== -1;
+};
+
+Appium.prototype.isChromeBrowser = function (browser) {
+  return _.contains(["chrome", "chromium", "chromebeta", "browser"], browser);
+};
+
+Appium.prototype.isChromePackage = function (pkg) {
+  var chromePkgs = [
+    "com.android.chrome"
+  , "com.chrome.beta"
+  , "org.chromium.chrome.shell"
+  , "com.android.browser"
+  ];
+  return _.contains(chromePkgs, pkg);
+};
+
+Appium.prototype.isSafariBrowser = function (browser) {
+  return browser === "safari";
+};
+
+Appium.prototype.getDeviceTypeFromPlatform = function (caps) {
   var device = null;
   switch (caps) {
   case 'ios':
@@ -207,63 +217,6 @@ Appium.prototype.getDeviceTypeFromPlatformCap = function (caps) {
     break;
   }
   return device;
-};
-
-Appium.prototype.getDeviceTypeFromDeviceCap = function (device) {
-
-  if (device.indexOf('iphone') !== -1) {
-    return DT_IOS;
-  } else if (device.indexOf('ipad') !== -1) {
-    return DT_IOS;
-  } else if (device.indexOf('selendroid') !== -1) {
-    return DT_SELENDROID;
-  } else if (device.indexOf('firefox') !== -1) {
-    return DT_FIREFOX_OS;
-  } else if (device.indexOf('android') !== -1) {
-    return DT_ANDROID;
-  }
-};
-
-Appium.prototype.getDeviceTypeFromNamedApp = function (app) {
-  if (app  === "safari") {
-    return DT_SAFARI;
-  } else if (app === "settings") {
-    return DT_IOS;
-  } else if (_.contains(["chrome", "chromium", "chromebeta", "browser"], app)) {
-    return DT_CHROME;
-  }
-};
-
-Appium.prototype.getDeviceTypeFromApp = function (app) {
-  if (/\.app$/.test(app) || /\.app\.zip$/.test(app)) {
-    return DT_IOS;
-  } else if (/\.apk$/.test(app) || /\.apk\.zip$/.test(app)) {
-    return DT_ANDROID;
-  }
-};
-
-Appium.prototype.getDeviceTypeFromPackage = function (pkg) {
-  var chromePkgs = [
-    "com.android.chrome"
-  , "com.chrome.beta"
-  , "org.chromium.chrome.shell"
-  , "com.android.browser"
-  ];
-  if (_.contains(chromePkgs, pkg)) {
-    return DT_CHROME;
-  } else if (pkg) {
-    return DT_ANDROID;
-  }
-};
-
-Appium.prototype.getDeviceTypeFromArgs = function (args) {
-  if (args.ipa && /\.ipa$/.test(args.ipa.toString().toLowerCase())) {
-    return DT_IOS;
-  } else if (args.safari) {
-    return DT_SAFARI;
-  } else if (args.forceIphone || args.forceIpad) {
-    return DT_IOS;
-  }
 };
 
 Appium.prototype.configure = function (args, desiredCaps, cb) {